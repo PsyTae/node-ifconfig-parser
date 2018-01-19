703c5030f172dc4a4f6235d9c6309c26455471e0
Shaun Branham <shaun.branham@gmail.com>
2018-01-19 21:36:09 +0000

    Mad some changes to allow for Parsing to a JSON Object istead of an array also fixed ether/mac getting overwritten

--- index.js
+++ index.js
@@ -1,78 +1,81 @@
-
-var Buffer = require('buffer-extend');
 var keys = [
-  'inet6',
-  'inet',
-  'prefixlen',
-  'netmask',
-  'scopeid',
-  'nd6',
-  'options',
-  'ether',
-  'broadcast',
-  'media:',
-  'status:',
-  'member:',
-  'id',
-  'priority',
-  'hellotime',
-  'fwddelay',
-  'maxage',
-  'holdcnt',
-  'proto',
-  'maxaddr',
-  'timeout',
-  'ipfilter',
-  'ifmaxaddr',
-  'flags'
+  "ether",
+  "inet",
+  "inet6",
+  "prefixlen",
+  "netmask",
+  "scopeid",
+  "nd6",
+  "options",
+  "broadcast",
+  "media:",
+  "status:",
+  "member:",
+  "id",
+  "priority",
+  "hellotime",
+  "fwddelay",
+  "maxage",
+  "holdcnt",
+  "proto",
+  "maxaddr",
+  "timeout",
+  "ipfilter",
+  "ifmaxaddr",
+  "flags",
+  "txqueuelen"
 ];
 
 function parse(src) {
-  var lines = src.split('\n');
+  var lines = src.split("\n");
   var blocks = [];
   var temp = [];
+  var ret = {};
   lines.forEach(function(item) {
-    if (['\t', ' '].indexOf(item[0]) === -1 && temp.length > 0) {
+    if (["\t", " "].indexOf(item[0]) === -1 && temp.length > 0) {
       blocks.push(temp);
       temp = [];
     }
-    temp.push(item);
+    if (item) {
+      temp.push(item);
+    }
   });
 
-  return blocks.map(function(block) {
-    var ret = {};
+  blocks.forEach(function(block) {
     var firstline = block[0].toString();
-    var coionIdx = firstline.indexOf(':');
+    var coionIdx = firstline.indexOf(":");
+    var name = firstline.slice(0, coionIdx);
     var conf = block.slice(1);
-    var flagsline = firstline.slice(coionIdx+1).trim();
-    var flagsMh = flagsline.match(/^flags=[0-9]+<{1}([A-Z,]*)>{1} mtu ([0-9]+)/);
+    var flagsline = firstline.slice(coionIdx + 1).trim();
+    var flagsMh = flagsline.match(
+      /^flags=[0-9]+<{1}([A-Z,]*)>{1}\s*mtu\s*([0-9]+)/
+    );
 
-    ret.name = firstline.slice(0, coionIdx-1);
-    ret.flags = flagsMh[1].split(',');
-    ret.mtu = parseInt(flagsMh[2], 10);
+    ret[name] = {};
+    ret[name].flags = flagsMh[1].split(",");
+    ret[name].mtu = parseInt(flagsMh[2], 10);
+
     conf.forEach(function(item) {
-      var list = item.split(' ').filter(function(item) {
+      var list = item.split(" ").filter(function(item) {
         return item.length > 0;
       });
       var key = null;
-      for (var i=0; i<list.length; i++) {
-        if (keys.indexOf(list[i]+'') !== -1) {
-          key = (list[i]+'').replace(':', '');
-          if (key && !ret[key]) ret[key] = true;
+      for (var i = 0; i < list.length; i++) {
+        if (keys.indexOf(list[i] + "") !== -1) {
+          key = (list[i] + "").replace(":", "");
           continue;
         }
         if (key) {
-          ret[key] = list[i]+'';
+          if (!ret[name][key]) ret[name][key] = list[i].toString();
         }
       }
     });
 
-    if (ret.flags.length && !ret.flags[0].length) {
-      ret.flags = [];
+    if (ret[name].flags.length && !ret[name].flags[0].length) {
+      ret[name].flags = [];
     }
-
-    return ret;
   });
+  return ret;
 }
 
 exports.parse = parse;
